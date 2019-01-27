fe52f580c9552b97a4ceb80aa1cd3d5cee06023c
Mukund <mukund0299@gmail.com>
2019-01-26 18:30:52 -0800

    Added global variables, get rows

--- backend/parser.py
+++ backend/parser.py
@@ -1,12 +1,13 @@
 import csv
 import os
+import config
 from datetime import date
 
 # Returns a Array[Column][Row] with all the information
 # from the data.csv file. Fixed 6 column, does not handle
 # errors
 def parseDataCSV():
-    data = [[] for x in range(0,6)]
+    config.data = [[] for x in range(0,6)]
     with open('data.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
@@ -19,17 +20,14 @@ def parseDataCSV():
                     asList = row[x].split('-')
                     converted = list(map(lambda x: int(x), asList))
                     row[x] = date(converted[2], converted[1], converted[0])
-                data[x].append(row[x])
-    return data
+                config.data[x].append(row[x])
 
 # Returns a dictionary with name:(lat, long)
 def parseLocationCSV():
-    data = {}
     with open('location.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            data[row[0]] = (float(row[1]), float(row[2]))
-    return data
+            config.locations[row[0]] = (float(row[1]), float(row[2]))
 
 
 
--- backend/stats.py
+++ backend/stats.py
@@ -1,30 +1,41 @@
 import parser
+import config
 from datetime import date
 
-# Global variables
-# data = []
-# locations = {}
+# Populates the data and locations variables
+def loadData():
+    parser.parseDataCSV()
+    parser.parseLocationCSV()
 
-# # Populates the data and locations variables
-# def loadData():
-#     data = parser.parseDataCSV()
-#     # locations = parser.parseLocationCSV()
+# Returns a tuple that has (startRow, endRow) from data
+# for specified dates
+def getRows(startDate, endDate):
+    startDateSplit = startDate.split('-')
+    endDateSplit = endDate.split('-')
+    startDateMod = list(map(lambda x: int(x), startDateSplit))
+    endDateMod = list(map(lambda x: int(x), endDateSplit))
+    startDateForm = date(startDateMod[2], startDateMod[1], startDateMod[0])
+    endDateForm = date(endDateMod[2], endDateMod[1], endDateMod[0]) 
+    numDays = (endDateForm-startDateForm).days + 1
+    startRow = 0
+    endRow = 0
+    for x in range(len(config.data[0])):
+        if (startDateForm < config.data[0][x]):
+            startRow = x - 1
+            break
+    for x in range(len(config.data[0])):
+        if (endDateForm < config.data[0][x]):
+            endRow = x - 1
+            break
+    return (startRow, endRow)
 
 # Assumes populated variables, startDate and endDate
 # produces a list of elements (i.e. startDate isn't 
 # at the end). Produces average spending per day in that range
 def avg(startDate, endDate):
     avg = 0
-    startDateSplit = startDate.split('-')
-    endDateSplit = endDate.split('-')
-    startDateMod= list(map(lambda x: int(x), startDateSplit))
-    endDateMod= list(map(lambda x: int(x), endDateSplit))
-    numDays = (date(endDateMod[2], endDateMod[1], endDateMod[0]) - date(startDateMod[2], startDateMod[1], startDateMod[0])).days
-    startRow = 0
-    endRow = 0
-    for x in range(len(data[0])):
-        if (startDate > date[0][x]):
-            startRow = x - 1
+    rows = getRows(startDate, endDate)
 
-data = parser.parseDataCSV()
-avg("01-01-2019", "06-01-2019")
+
+loadData()
+avg("02-01-2019", "06-01-2019")
