649a8a3e393f899f07ee8dfe6258c94c0f646fbd
Mukund <mukund0299@gmail.com>
2019-01-26 17:42:22 -0800

    Completed parsers, data structures, started avg

--- backend/parser.py
+++ backend/parser.py
@@ -1,18 +1,35 @@
 import csv
 import os
+from datetime import date
 
-
-def parse():
+# Returns a Array[Column][Row] with all the information
+# from the data.csv file. Fixed 6 column, does not handle
+# errors
+def parseDataCSV():
     data = [[] for x in range(0,6)]
     with open('data.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        flag = False
         for row in reader:
-            if (not flag):
-                flag = True
-                continue
-            # print(row)
             for x in range(0, 6):
+                if (x == 4):
+                    row[x] = int(row[x])
+                elif (x == 3 or x == 2):
+                    row[x] = float(row[x])
+                elif (x == 0):
+                    asList = row[x].split('-')
+                    converted = list(map(lambda x: int(x), asList))
+                    row[x] = date(converted[2], converted[1], converted[0])
                 data[x].append(row[x])
     return data
+
+# Returns a dictionary with name:(lat, long)
+def parseLocationCSV():
+    data = {}
+    with open('location.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            data[row[0]] = (float(row[1]), float(row[2]))
+    return data
+
+
 
--- backend/stats.py
+++ backend/stats.py
@@ -1,8 +1,30 @@
 import parser
+from datetime import date
 
+# Global variables
+# data = []
+# locations = {}
 
+# # Populates the data and locations variables
+# def loadData():
+#     data = parser.parseDataCSV()
+#     # locations = parser.parseLocationCSV()
 
+# Assumes populated variables, startDate and endDate
+# produces a list of elements (i.e. startDate isn't 
+# at the end). Produces average spending per day in that range
+def avg(startDate, endDate):
+    avg = 0
+    startDateSplit = startDate.split('-')
+    endDateSplit = endDate.split('-')
+    startDateMod= list(map(lambda x: int(x), startDateSplit))
+    endDateMod= list(map(lambda x: int(x), endDateSplit))
+    numDays = (date(endDateMod[2], endDateMod[1], endDateMod[0]) - date(startDateMod[2], startDateMod[1], startDateMod[0])).days
+    startRow = 0
+    endRow = 0
+    for x in range(len(data[0])):
+        if (startDate > date[0][x]):
+            startRow = x - 1
 
-
-
-
+data = parser.parseDataCSV()
+avg("01-01-2019", "06-01-2019")
